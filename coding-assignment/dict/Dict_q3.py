92b0fb5515f3ddb66c568fb78cc594e647a22388
thakurnikhilsingh1 <nikhilks2106@gmail.com>
2024-11-27 23:16:02 +0530

    Check if a key exists

---
+++ coding-assignment/dict/Dict_q3.py
@@ -0,0 +1,8 @@
+# Check if a key exists
+data = {'name': 'Bob', 'age': 30, 'city': 'Chicago'}
+key_to_check = 'age'
+if key_to_check in data:
+    print(f"'{key_to_check}' exists in the dictionary.")
+else:
+    print(f"'{key_to_check}' does not exist.")
+
