742f39c77b66d3cc01bc86025bdee2f9d96af947
thakurnikhilsingh1 <nikhilks2106@gmail.com>
2024-11-27 23:48:45 +0530

    Check Prime

---
+++ coding-assignment/function/function_q3.py
@@ -0,0 +1,11 @@
+# Check Prime
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+print(is_prime(7))  
+print(is_prime(10))  
